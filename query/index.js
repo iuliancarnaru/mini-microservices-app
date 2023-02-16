3b4e04969c2ca21003cadd2f872075616c8371ab
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-02-16 11:51:15 +0000

    query service

--- query/index.js
+++ query/index.js
@@ -7,8 +7,28 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
-app.post('/events', (req, res) => {});
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
 
 app.listen(PORT, () => {
   console.log(`Query service running on port: ${PORT}`);
