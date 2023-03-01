6c57249b122bc52ae04cb46da7beb61882128579
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-01 22:23:55 +0000

    added ingress

--- client/src/PostList.jsx
+++ client/src/PostList.jsx
@@ -8,7 +8,7 @@ const PostList = () => {
 
   const fetchPosts = async () => {
     // QUERY service
-    const res = await axios.get('http://localhost:4002/posts');
+    const res = await axios.get('http://posts.com/posts');
     setPosts(res.data);
   };
 
--- posts/index.js
+++ posts/index.js
@@ -12,11 +12,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.status(200).send(posts);
-});
-
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
